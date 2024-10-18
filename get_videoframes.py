de6896da2beb83be840863ffcb26791e1c8a434f
Timofey M <agentmorosha@gmail.com>
2024-10-18 17:55:20 +0400

    load dataset

--- get_videoframes.py
+++ get_videoframes.py
@@ -2,11 +2,12 @@ import cv2
 capture = cv2.VideoCapture('video/1e7d9cad-ad5a-4ca5-9d5a-0ca5c4089ddd.avi')
 frameNr = 0
 while (True):
- 
     success, frame = capture.read()
- 
+    n = 1
+
     if success:
-            cv2.imwrite(f'allFrames/frame_{frameNr}.jpg', frame)
+            if frameNr % n == 0:
+                cv2.imwrite(f'images/frame_{frameNr // n}.jpg', frame)
     else:
         break
  
